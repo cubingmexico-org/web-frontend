03ef19213cb7dce080d86f221485b51dd4b56cb5
Leonardo Sánchez Del Toro <leotorokr@gmail.com>
2024-04-18 21:06:40 -0700

    feat(sponsors-cup):  add CRON_SECRET env variable

--- apps/sponsors-cup/src/app/api/competitions-cron/route.ts
+++ apps/sponsors-cup/src/app/api/competitions-cron/route.ts
@@ -1,8 +1,16 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type -- . */
+import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import { insertCompetitions } from '@/app/lib/data';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
+  
   await insertCompetitions();
   return NextResponse.json({ success: true });
 }
--- apps/sponsors-cup/src/app/api/new-scores-cron/route.ts
+++ apps/sponsors-cup/src/app/api/new-scores-cron/route.ts
@@ -1,8 +1,16 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type -- . */
+import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import { insertNewScores } from '@/app/lib/data';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
+
   await insertNewScores();
   return NextResponse.json({ success: true });
 }

ea151ce7915c9b6c1b0b373fc0d3a173d79ca225
Leonardo Sánchez Del Toro <leotorokr@gmail.com>
2024-06-02 13:12:04 -0700

    feat(wca-certificates): add page configuration dialog

--- apps/wca-certificates/src/components/document-form.tsx
+++ apps/wca-certificates/src/components/document-form.tsx
@@ -8,10 +8,26 @@ import React, { useState, useEffect } from 'react'
 import type { JSONContent } from '@tiptap/react'
 import { Button } from "@repo/ui/button"
 import { Label } from "@repo/ui/label";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@repo/ui/select";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@repo/ui/dialog"
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem
+} from '@repo/ui/select';
+import { RadioGroup, RadioGroupItem } from "@repo/ui/radio-group"
+import { Input } from '@repo/ui/input';
 import * as pdfMake from "pdfmake/build/pdfmake";
 import * as pdfFonts from 'pdfmake/build/vfs_fonts';
-import type { TDocumentDefinitions } from 'pdfmake/interfaces';
+import type { Margins, TDocumentDefinitions } from 'pdfmake/interfaces';
+import { FileText } from 'lucide-react';
 import { FileUploader } from "@/components/file-uploader";
 import { podium } from '@/lib/placeholders';
 import Tiptap from './editor/tiptap'
@@ -35,9 +51,10 @@ const fonts = {
 
 export default function DocumentForm(): JSX.Element {
 
-
-  const [pageSize, setPageSize] = useState<"LETTER" | "A4">("LETTER");
+  const [pageMargins, setPageMargins] = useState<Margins>([60, 40, 60, 40]);
   const [pageOrientation, setPageOrientation] = useState<"portrait" | "landscape">("portrait");
+  const [pageSize, setPageSize] = useState<"LETTER" | "A4">("LETTER");
+
   const [files, setFiles] = useState<File[]>([]);
   const [background, setBackground] = useState<string>();
 
@@ -87,38 +104,38 @@ export default function DocumentForm(): JSX.Element {
 
   const renderTextContent = (content: JSONContent['content']) => {
     return content?.map((contentItem) => {
-      const isBold = contentItem.marks?.some(mark => mark.type === 'bold');
-      const fontFamily = contentItem.marks?.find(mark => mark.type === 'textStyle')?.attrs?.fontFamily;
+      const bold = contentItem.marks?.some(mark => mark.type === 'bold');
+      const font = contentItem.marks?.find(mark => mark.type === 'textStyle')?.attrs?.fontFamily;
 
       const textObject = (text: string | undefined) => ({
         text,
-        bold: isBold,
-        font: fontFamily,
+        bold,
+        font,
       });
 
       switch (contentItem.type) {
         case 'text':
-          return isBold || fontFamily ? textObject(contentItem.text) : contentItem.text;
+          return bold || font ? textObject(contentItem.text) : contentItem.text;
         case 'mention':
           switch (contentItem.attrs?.id) {
             case 'Delegados':
-              return isBold || fontFamily ? textObject(getDelegatesNames()) : getDelegatesNames();
+              return bold || font ? textObject(getDelegatesNames()) : getDelegatesNames();
             case 'Organizadores':
-              return isBold || fontFamily ? textObject(getOrganizersNames()) : getOrganizersNames();
+              return bold || font ? textObject(getOrganizersNames()) : getOrganizersNames();
             case 'Lugar':
-              return isBold || fontFamily ? textObject('Tercer Lugar') : 'Tercer Lugar';
+              return bold || font ? textObject('Tercer Lugar') : 'Tercer Lugar';
             case 'Competidor':
-              return isBold || fontFamily ? textObject('Leonardo Sánchez Del Toro') : 'Leonardo Sánchez Del Toro';
+              return bold || font ? textObject('Leonardo Sánchez Del Toro') : 'Leonardo Sánchez Del Toro';
             case 'Evento':
-              return isBold || fontFamily ? textObject('Megaminx') : 'Megaminx';
+              return bold || font ? textObject('Megaminx') : 'Megaminx';
             case 'Resultado':
-              return isBold || fontFamily ? textObject('49.00') : '49.00';
+              return bold || font ? textObject('49.00') : '49.00';
             case 'Competencia':
-              return isBold || fontFamily ? textObject('Nayar Open 2024') : 'Nayar Open 2024';
+              return bold || font ? textObject('Nayar Open 2024') : 'Nayar Open 2024';
             case 'Fecha':
-              return isBold || fontFamily ? textObject('23 y 24 de marzo de 2024') : '23 y 24 de marzo de 2024';
+              return bold || font ? textObject('23 y 24 de marzo de 2024') : '23 y 24 de marzo de 2024';
             case 'Ciudad':
-              return isBold || fontFamily ? textObject('Tepic, Nayarit') : 'Tepic, Nayarit';
+              return bold || font ? textObject('Tepic, Nayarit') : 'Tepic, Nayarit';
             default:
               return null;
           }
@@ -139,9 +156,19 @@ export default function DocumentForm(): JSX.Element {
   const generatePDF = () => {
     const docDefinition = {
       content: renderContent(content),
+      background(currentPage, pageSize) {
+        if (background) {
+          return {
+            image: background,
+            width: pageSize.width,
+            height: pageSize.height
+          }
+        }
+        return null;
+      },
+      pageMargins,
       pageOrientation,
       pageSize,
-      pageMargins: [40, 60, 40, 60],
       styles: {
         header1: {
           fontSize: 24
@@ -156,16 +183,7 @@ export default function DocumentForm(): JSX.Element {
           fontSize: 12
         }
       },
-      background (currentPage, pageSize) {
-        if (background) {
-          return {
-            image: background,
-            width: pageSize.width,
-            height: pageSize.height
-          }
-        }
-        return null;
-      },
+      language: 'es'
     } as TDocumentDefinitions;
 
     pdfMake.createPdf(docDefinition, undefined, fonts, vfs).open();
@@ -174,41 +192,113 @@ export default function DocumentForm(): JSX.Element {
   return (
     <div>
       <div className='flex justify-center'>
-        <div className='w-full pr-1'>
-          <Label>Tamaño de hoja</Label>
-          <Select onValueChange={(value: "LETTER" | "A4") => { setPageSize(value); }} value={pageSize}>
-            <SelectTrigger className="w-full">
-              <SelectValue placeholder="Tamaño *" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="LETTER">Carta</SelectItem>
-              <SelectItem value="A4">A4</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-        <div className='w-full pl-1'>
-          <Label>Orientación de hoja</Label>
-          <Select onValueChange={(value: "portrait" | "landscape") => { setPageOrientation(value); }} value={pageOrientation}>
-            <SelectTrigger className="w-full">
-              <SelectValue placeholder="Orientación *" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="landscape">Horizontal</SelectItem>
-              <SelectItem value="portrait">Vertical</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button><FileText className='h-5 w-5 mr-2' /> Configuración de página</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Configuración de página</DialogTitle>
+              <div className='grid grid-cols-2 gap-2'>
+                <div>
+                  <div>
+                    <Label htmlFor='pageOrientation'>Orientación</Label>
+                    <RadioGroup className='flex' defaultValue="portrait" id='pageOrientation' onValueChange={(value: "portrait" | "landscape") => { setPageOrientation(value); }} value={pageOrientation}>
+                      <div className="flex items-center space-x-2">
+                        <RadioGroupItem id="portrait" value="portrait" />
+                        <Label htmlFor="portrait">Vertical</Label>
+                      </div>
+                      <div className="flex items-center space-x-2">
+                        <RadioGroupItem id="landscape" value="landscape" />
+                        <Label htmlFor="landscape">Horizontal</Label>
+                      </div>
+                    </RadioGroup>
+                  </div>
+                  <div>
+                    <Label htmlFor='pageSize'>Tamaño de papel</Label>
+                    <Select onValueChange={(value: "LETTER" | "A4") => { setPageSize(value); }} value={pageSize}>
+                      <SelectTrigger className="w-full" id='pageSize'>
+                        <SelectValue placeholder="Tamaño *" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="LETTER">Carta</SelectItem>
+                        <SelectItem value="A4">A4</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </div>
+                <div>
+                  <Label htmlFor='pageMargins'>Márgenes</Label>
+                  <div>
+                    <div className='flex items-center gap-2'>
+                      <Label htmlFor='top'>Superior</Label>
+                      <Input
+                        className='w-full'
+                        id='top'
+                        min={0}
+                        onChange={(e) => {
+                          setPageMargins([parseInt(e.target.value), pageMargins[1], pageMargins[2], pageMargins[3]]);
+                        }}
+                        type='number'
+                        value={pageMargins[0]}
+                      />
+                    </div>
+                    <div className='flex items-center gap-2'>
+                      <Label htmlFor='bottom'>Inferior</Label>
+                      <Input
+                        className='w-full'
+                        id='bottom'
+                        min={0}
+                        onChange={(e) => {
+                          setPageMargins([pageMargins[0], pageMargins[1], parseInt(e.target.value), pageMargins[3]]);
+                        }}
+                        type='number'
+                        value={pageMargins[2]}
+                      />
+                    </div>
+                    <div className='flex items-center gap-2'>
+                      <Label htmlFor='right'>Derecho</Label>
+                      <Input
+                        className='w-full'
+                        id='right'
+                        min={0}
+                        onChange={(e) => {
+                          setPageMargins([pageMargins[0], parseInt(e.target.value), pageMargins[2], pageMargins[3]]);
+                        }}
+                        type='number'
+                        value={pageMargins[1]}
+                      />
+                    </div>
+                    <div className='flex items-center gap-2'>
+                      <Label htmlFor='left'>Izquierdo</Label>
+                      <Input
+                        className='w-full'
+                        id='left'
+                        min={0}
+                        onChange={(e) => {
+                          setPageMargins([pageMargins[0], pageMargins[1], pageMargins[2], parseInt(e.target.value)]);
+                        }}
+                        type='number'
+                        value={pageMargins[3]}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </div>
-        <div className='my-4'>
-          <Label htmlFor='background'>Fondo</Label>
-          <FileUploader
-            id='background'
-            maxFiles={1}
-            maxSize={1 * 1024 * 1024}
-            onValueChange={(e) => { setFiles(e); }}
-            value={files}
-          />
-        </div>
+      <div className='my-4'>
+        <Label htmlFor='background'>Fondo</Label>
+        <FileUploader
+          id='background'
+          maxFiles={1}
+          maxSize={1 * 1024 * 1024}
+          onValueChange={(e) => { setFiles(e); }}
+          value={files}
+        />
+      </div>
       <form
         className="grid place-items-center mx-auto pt-10 mb-10"
         onSubmit={handleSubmit}
